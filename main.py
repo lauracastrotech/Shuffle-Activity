51919d307f5aa715208c96bc910dcafe57f71211
Laura Castro <lauracastrotech@gmail.com>
2025-03-17 20:05:08 -0400

    edit shuffle_deck function to return shuffled deck

--- main.py
+++ main.py
@@ -5,20 +5,16 @@ SUITS = ["♥", "♦", "♠", "♣"]
 
 deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 
-# Implement a solution for shuffling a deck of cards
-# Use only randint() and no other imports
 def shuffle_deck(deck_of_cards):
-    # Loop through deck
+    print(f"{deck_of_cards=}")
     for card in range(len(deck_of_cards)):
         random_card = randint(0, len(deck_of_cards) - 1)
-        print(f"{random_card=}")
-        print(f"{len(deck_of_cards)=}")
-        current_card = deck_of_cards[card]
-        deck_of_cards[card] = deck_of_cards[random_card]
-        deck_of_cards[random_card] = current_card
 
-    # Generate a random index
-    # Assign current index to temp variable
-    # Assign element at random index to current index
-    # Assign temp variable to random index
-shuffle_deck(deck_of_cards)
+        current_card = deck_of_cards[card]
+
+        deck_of_cards[card] = deck_of_cards[random_card]
+
+        deck_of_cards[random_card] = current_card
+    return deck_of_cards
+    
+print(shuffle_deck(deck_of_cards))
